f1318408e36ef77de14715bb7c9b043f30176e3f
Choi-UngGyu <cdr982116@gmail.com>
2020-12-03 09:37:06 +0900

    0.13 Conclusions

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,7 @@ const createNewBlock = (data: string): Block => {
     const newTimestamp: number = getNewTimeStamp();
     const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
     const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
     return newBlock;
 };
 
@@ -76,5 +77,11 @@ const addBlock = (candidateBlock: Block): void => {
         blockchain.push(candidateBlock);
     }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
