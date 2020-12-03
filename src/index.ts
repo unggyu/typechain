00da277c5b8711bea490cbe1f11da2f62b3aa81b
Choi-UngGyu <cdr982116@gmail.com>
2020-12-03 09:25:15 +0900

    0.11 Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import * as CryptoJS from 'crypto-js';
 
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-
     static calculateBlockHash = (
         index: number,
         previousHash: string,
         timestamp: number,
         data: string
     ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
     constructor(
         index: number,
@@ -48,6 +55,14 @@ const createNewBlock = (data: string): Block => {
     return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if ()
+}
 
 export {};
